643f8792b8be8b1e9ebc634d10434f4a43519145
sano820 <sano820@naver.com>
2026-02-04 12:48:37 +0900

    주석으로 각 코드 내용 설명 작업자 : 박상선

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// React 앱을 브라우저 DOM에 연결하고 렌더링을 시작하는 진입 파일
+
+import { StrictMode } from 'react' // 개발단계에서 잠재적인 문제를 미리 집기 위한 도구 => 일부 라이프사이클 로직을 의도적으로 두번 실행해 문제 코드 탐지
+import { createRoot } from 'react-dom/client'  
 import './index.css'
 import App from './App.jsx'
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,22 @@
+/*
+- Vite 개발 서버 및 빌드 동작을 설정하는 파일
+- frontend 개발 환경의 “인프라 설정”에 해당
+*/
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // react 관련 기능 활성화  
+  //jsx 문법을 부라우저가 이해할 수 있는 js로 변환
+  plugins: [react()],  // 코드 수정시 화면이 즉시 갱신되는 개발 환경 제공
   server: {
     host: '0.0.0.0',
     port: 3000,
+    /*
+    proxy를 설정 => 개발환경에서 frontend와 backend를 하나의 서버처럼 사용하기 위함.
+    */
     proxy: {
       "/api": {
         target: "http://localhost:8000",
